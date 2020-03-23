466c1ab8b59baa6f3d0a0c7aec79488e057a0c6f
mdrokz <mohammadmunshi@gmail.com>
2020-03-23 11:54:02 +0530

    Alpha Commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate serde_derive;
+
+mod utils;
+
+mod types;
+
+use wasm_bindgen::prelude::*;
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s:&str);
+}
+
+#[wasm_bindgen]
+pub async fn getCoronaData() -> String {
+    log("running");
+    let data = getData().await;
+    data
+}
+
+async fn getData() -> String {
+    let res = reqwest::get("https://coronavirus.m.pipedream.net").await.unwrap();
+    res.text().await.unwrap()
+    //String::from("hhe")
+}
